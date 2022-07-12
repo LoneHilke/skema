1f2b94f135069ac60e98f0a6f88091cee6ff9df1
Lone Hilke <lonehilke@gmail.com>
2022-07-12 22:51:00 +0200

    skema 2
    skema 2
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- myworld/skema/views.py
+++ myworld/skema/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Skema
+
 
 # Create your views here.
 def index(request):
-  template = loader.get_template('my_first.html')
-  return HttpResponse(template.render())
+    myskema = Skema.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+    'myskema': myskema
+   }
+    return HttpResponse(template.render(context, request))
+  
+  #template = loader.get_template('my_first.html')
+  #return HttpResponse(template.render())
 
 
