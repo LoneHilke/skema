b00af26e59cd27e4232c661a3f30fd6688b50d10
Lone Hilke <lonehilke@gmail.com>
2022-07-16 23:16:31 +0200

    har prøvet deploy
    problemer med deploy
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'skema.apps.SkemaConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
+import os
+if 'ON_HEROKU' in os.environ:
+    ALLOWED_HOSTS.append('tlf-skema.herokuapp.com')
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    DEBUG = os.getenv('DEBUG') == 'TRUE'
+    SECRET_KEY = os.getenv('SECRET_KEY')
